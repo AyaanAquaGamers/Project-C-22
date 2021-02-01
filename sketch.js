31a5a51f73a985ea2ee8660a9ee50887ef9c1f08
AyaanAquaGamers <72394681+AyaanAquaGamers@users.noreply.github.com>
2021-02-01 17:24:44 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
-var line1,line2,line3
 var packageBody,ground
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var line2,line2a
+var line1,line1a
+var line3,line3a
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -15,26 +17,29 @@ function preload()
 function setup() {
 	createCanvas(800, 700);
 	rectMode(CENTER);
-	
 	engine = Engine.create();
 	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 250 , 5 , {restitution:0.7, isStatic:true});
+	World.add(world, packageBody);
+
+	packageSprite=createSprite(width/2,250, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
 
 	line1=Bodies.rectangle(310,620,20,100,{isStatic:true})
 	fill("red")
 	World.add(world,line1)
 
-	line2=Bodies.rectangle(400,650,200,20,{isStatic:true})
-	fill("red")
-	World.add(world,line2)
-
+	line1a=createSprite(310,620,20,100)
+	line1a.shapeColor="red"
+	
 	line3=Bodies.rectangle(490,620,20,100,{isStatic:true})
 	fill("red")
-	World.add(world,line3)
-	
+	World.add(world,line3)	
 
-	packageSprite=createSprite(width/2,250, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
+	line3a=createSprite(490,620,20,100)
+	line3a.shapeColor="red"
 
 	helicopterSprite=createSprite(width/2, 250, 10,10);
 	helicopterSprite.addImage(helicopterIMG)
@@ -45,23 +50,29 @@ function setup() {
 	
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
+  
+	 line2=Bodies.rectangle(400,650,200,20,{isStatic:true})
 
-	packageBody = Bodies.circle(width/2 , 250 , 5 , {restitution:0.5, isStatic:true});
-	World.add(world, packageBody);
+	fill("red")
+	World.add(world,line2)
+
+	line2a=createSprite(400,650,200,20)
+	line2a.shapeColor="red"
 
 	Engine.run(engine);
   console.log(line1)
+  
 }
 
 
 function draw() {
   rectMode(CENTER);
   background(0);
+  packageSprite.x=packageBody.position.x
+  packageSprite.y=packageBody.position.y 
   rect(line1.position.x,line1.position.y,20,100)
   rect(line2.position.x,line2.position.y,200,20)
   rect(line3.position.x,line3.position.y,20,100)
-  packageSprite.x=packageBody.position.x
-  packageSprite.y=packageBody.position.y 
   drawSprites();
 }
 
